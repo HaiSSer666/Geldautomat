e4e118ef25258e5061cbeb5b60321e262cf2ec91
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-02-01 23:09:00 +0100

    Improve readability

--- src/main1/Main.java
+++ src/main1/Main.java
@@ -7,17 +7,22 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Bankomat b1 = new Bankomat();
+		
 		b1.accept(new Pyatihatka(true, 80));
 		b1.accept(new Poltos(true, 88));
 		System.out.println(b1.commitCurrentTransaction());
+		
 		b1.accept(new Poltos(true, 80));
 		b1.accept(new Poltos(false, 88));
 		System.out.println(b1.commitCurrentTransaction());
+		
 		b1.accept(new Pyatihatka(true, 55));
 		System.out.println(b1.commitCurrentTransaction());
+		
 		b1.accept(new Poltos(true, 80));
 		b1.accept(new Poltos(true, 88));
 		System.out.println(b1.declineCurrentTransaction());
+		
 		System.out.println(b1.getTotalAmount());
 	}
 }
